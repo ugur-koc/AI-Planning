c0f0b52a0bfe8758c2fbe76c51d75048315d881a
Ugur <ugurkoc.uk@hotmail.com>
2015-04-16 12:25:59 -0400

    fixing syntax issues
    fixing syntax issues

--- src/acting/Actor.java
+++ src/acting/Actor.java
@@ -2,80 +2,84 @@ package acting;
 
 import java.util.ArrayList;
 
-
-import planning.core.*;
+import planning.core.Action;
+import planning.core.Plan;
+import planning.core.Planner;
+import planning.core.Problem;
+import planning.core.State;
+import planning.core.StateTransitionSystem;
 import planning.utility.Helper;
-import exceptions.NoPlanException;
 
 public class Actor {
-	
-	public static Plan solve(Problem problem, String refinement){
-		//try{
-			if (refinement.equals("ARP_lazy")) return ARP_lazy(problem);
-			else if (refinement.equals("ARP_interleaved")) return ARP_interleaved(problem);
-			else  return ARP_mixed(problem);
-			// else 
-		//}catch (l) {
-		//}
-	}
-		
 
-	public static Plan ARP_lazy(Problem problem){  // actually AP_lazy
-		State s = problem.getInitialState();
-		
-		while(!Helper.satifies(s, problem.getGoalState())){
-			Plan plan=Planner.solve(problem, "Astar");
-			while (Simulate(problem, s,plan,problem.getGoalState())){
-			ArrayList<Action>  actions =plan.getActions(); 
-			s=problem.getSystem().transition(s, actions.get(0)); // the actor's transition system and planner's transition system are same in this case.
-			plan.removeAction(actions.get(0));
-			StateTransitionSystem ST= new StateTransitionSystem();
-			Problem P =Problem(ST, s, problem.getGoalState());
-			problem =P;
-			}
-				
-		}
+	public static void act(Problem problem, String refinement) {
+		if (refinement.equals("ARP_lazy")) ARP_lazy(problem);
+		else if (refinement.equals("ARP_interleaved")) ARP_interleaved(problem);
+		else ARP_mixed(problem);
 	}
 
+	public static void ARP_lazy(Problem problem) {
+		// actually AP_lazy
+		State s = problem.getInitialState();
 
-	public static Plan ARP_interleaved(Problem problem){ // actually AP_interleaved
-		State s = problem.getInitialState();
-		while(!Helper.satifies(s, problem.getGoalState())){
-			StateTransitionSystem ST= new StateTransitionSystem();
-			Problem P =Problem(ST, s, problem.getGoalState());
-			Plan plan = Planner.solve(P, "AStar");
-			ArrayList<Action>  actions =plan.getActions(); 
-			s=P.getSystem().transition(s, actions.get(0)); // the actor's transition system and planner's transition system are same in this case.
-		}
-	}
-	
-	public static Plan ARP_mixed(Problem problem){ // Actuallly AP_mixed
-		int n=5; int i=0;
-		State s = problem.getInitialState();
-		while(!Helper.satifies(s, problem.getGoalState())){
-			Plan plan=Planner.solve(problem, "Astar");
-			while(i++<n && Simulate(problem, s,plan,problem.getGoalState())){
-				ArrayList<Action>  actions =plan.getActions(); 
-				s=problem.getSystem().transition(s, actions.get(0)); // the actor's transition system and planner's transition system are same in this case.
+		while (!Helper.satifies(s, problem.getGoalState())) {
+			Plan plan = Planner.solve(problem, "Astar");
+			while (Simulate(problem, s, plan, problem.getGoalState())) {
+				ArrayList<Action> actions = plan.getActions();
+				// the actor's transition system and planner's transition system are
+				// same in this case.
+				s = problem.getSystem().transition(s, actions.get(0));
 				plan.removeAction(actions.get(0));
-				StateTransitionSystem ST= new StateTransitionSystem();
-				Problem P =Problem(ST, s, problem.getGoalState());
-				problem =P;
+				StateTransitionSystem ST = new StateTransitionSystem();
+				Problem P = new Problem(ST, s, problem.getGoalState());
+				problem = P;
+			}
+
+		}
+	}
+
+	public static void ARP_interleaved(Problem problem) {
+		// actually AP_interleaved
+		State s = problem.getInitialState();
+		while (!Helper.satifies(s, problem.getGoalState())) {
+			StateTransitionSystem ST = new StateTransitionSystem();
+			Problem P = new Problem(ST, s, problem.getGoalState());
+			Plan plan = Planner.solve(P, "AStar");
+			ArrayList<Action> actions = plan.getActions();
+			// the actor's transition system and planner's transition system are
+			// same in this case.
+			s = P.getSystem().transition(s, actions.get(0));
+		}
+	}
+
+	public static void ARP_mixed(Problem problem) { // Actuallly AP_mixed
+		int n = 5;
+		int i = 0;
+		State s = problem.getInitialState();
+		while (!Helper.satifies(s, problem.getGoalState())) {
+			Plan plan = Planner.solve(problem, "Astar");
+			while (i++ < n && Simulate(problem, s, plan, problem.getGoalState())) {
+				ArrayList<Action> actions = plan.getActions();
+				s = problem.getSystem().transition(s, actions.get(0));
+				// the actor's transition system and planner's transition system are
+				// same in this case.
+				plan.removeAction(actions.get(0));
+				StateTransitionSystem ST = new StateTransitionSystem();
+				Problem P = new Problem(ST, s, problem.getGoalState());
+				problem = P;
 			}
 		}
 	}
-	
+
 	private static boolean Simulate(Problem problem, State s, Plan plan, State goalState) {
-		State g =s;
-		ArrayList<Action> actions =plan.getActions();
-		for (Action a: actions){
-			g=problem.getSystem().transition(s, a);
-			s=g;
+		State g = s;
+		ArrayList<Action> actions = plan.getActions();
+		for (Action a : actions) {
+			g = problem.getSystem().transition(s, a);
+			s = g;
 		}
 		if (Helper.satifies(s, goalState)) return true;
 		else return false;
 	}
-
-
 
 }
